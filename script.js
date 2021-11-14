b05aaebb11cb77c30835d1a58f2e1d8f84b93bdf
ritsuri <93442009+ritsuri@users.noreply.github.com>
2021-11-14 13:23:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,27 @@
 
 function Cal () {
 
-  var a = document.getElementById("Amount").value;
-  var b = document.getElementById("Irate").value;
-  var c = parseInt(document.getElementById("Years").value);
-
-if(isNaN(a)) {return alert("Please input number only");}
-
-  let result = (a*b*c)/100;
-  let futureyear = 2020+c;
-
-    document.getElementById("resultAmount").innerHTML = "If you deposit " + a + ",";
-    document.getElementById("resultIrate").innerHTML = "At an interest rate of " + b + "%.";
-    document.getElementById("resultAnswer").innerHTML = "You will receive an amount of " + result + ",";
-    document.getElementById("resultYears").innerHTML = "in the year " + futureyear + ".";
-
+    var a = document.getElementById("Amount").value;
+    var b = document.getElementById("Irate").value;
+    var c = parseInt(document.getElementById("Years").value);
+  
+  if(isNaN(a) || a<=0) {return alert("Enter a positive number");}
+  
+    let result = (a*b*c)/100;
+    let futureyear = 2021+c;
+  
+      document.getElementById("resultAmount").innerHTML = "If you deposit " + a + ",";
+      document.getElementById("resultIrate").innerHTML = "At an interest rate of " + b + "%.";
+      document.getElementById("resultAnswer").innerHTML = "You will receive an amount of " + result + ",";
+      document.getElementById("resultYears").innerHTML = "in the year " + futureyear + ".";
+  
+    }
+  
+  var rangeslider = document.getElementById("Irate");
+  var output = document.getElementById("demo");
+  output.innerHTML = rangeslider.value + "%";
+  
+  rangeslider.oninput = function() {
+    output.innerHTML = this.value + "%";
   }
-
-var rangeslider = document.getElementById("Irate");
-var output = document.getElementById("demo");
-output.innerHTML = rangeslider.value + "%";
-
-rangeslider.oninput = function() {
-  output.innerHTML = this.value + "%";
-}
+  
